1730b194da56edf9a810a4d2ba5c3cbfd17cd1fb
abdurrehman99 <syed.ebad1999@gmail.com>
2021-05-23 02:31:27 +0500

    task 2 completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("task");
+(function task2() {
+	const addNewButton = document.querySelector(".add-new-btn");
+	const inputBox = document.querySelector(".container-input");
+
+	//event listener for button click
+	addNewButton.addEventListener("click", () => {
+		//create input element
+		const newInput = document.createElement("input");
+		//adding atttributes
+		newInput.type = "text";
+		newInput.className = "input-text";
+		//put it in DOM
+		inputBox.appendChild(newInput);
+	});
+})();
