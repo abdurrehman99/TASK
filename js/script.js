f56d845735458e69d69007ec7540f7dc6038ba75
abdurrehman99 <syed.ebad1999@gmail.com>
2021-05-23 03:06:41 +0500

    task 3 done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 (function task2() {
-	const addNewButton = document.querySelector(".add-new-btn");
+	//getting elements from DOM
+	const addNewButton = document.querySelector(".add-new-button");
 	const inputBox = document.querySelector(".container-input");
 
 	//event listener for button click
@@ -11,5 +12,30 @@
 		newInput.className = "input-text";
 		//put it in DOM
 		inputBox.appendChild(newInput);
+	});
+})();
+
+(function task3() {
+	//getting elements from DOM
+	const reverseStringInput = document.querySelector(".reverse-string-input");
+	const reverseStringButton = document.querySelector(".reverse-string-button");
+	const reverseString = document.querySelector(".reverse-string");
+
+	//stack initilization
+	let stack = [];
+
+	//event listener for button click
+	reverseStringButton.addEventListener("click", () => {
+		//push all chars into stack
+		for (let i = 0; i < reverseStringInput.value.length; i++) {
+			stack.push(reverseStringInput.value[i]);
+		}
+		//pop all chars to get reveresed string
+		let reversedValue = "";
+		for (let i = 0; i < reverseStringInput.value.length; i++) {
+			reversedValue += stack.pop(reverseStringInput.value[i]);
+		}
+		reverseString.innerHTML = reversedValue;
+		reverseStringInput.value = "";
 	});
 })();
